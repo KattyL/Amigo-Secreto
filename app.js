ca46fda82f095b4e390105b4dd6ef25db0634147
KattyL <katturync@gmail.com>
2025-01-22 01:39:39 -0300

    Salvando projeto no GitHub

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+let exibirAmigos = document.querySelector('.name-list');
+let sortear = document.querySelector('.button-draw')
+
+
+
+function criarLista(){
+    exibirAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement('li')
+        item.textContent = amigos[i]
+        exibirAmigos.appendChild(item);
+    }
+}
+function adicionarAmigo(){
+    let nome = document.querySelector('.input-name').value;
+    if (nome !== '') {
+        amigos.push(nome);
+        console.log(amigos);
+        document.querySelector('.input-name').value = '';
+        criarLista()
+        verificarLista()
+        
+      } else {
+        alert('Por favor, insira um nome válido!');
+      }
+    } 
+
+function verificarLista(){
+
+    if (amigos.length >= 2){
+        sortear.disabled = false;
+    } else {
+        sortear.disabled = true;
+    }
+}
+
+function sortearAmigo(){
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    exibirAmigos.innerHTML = 'Nada de spoilers!🤫 Seu amigo secreto é...'
+    campo = document.getElementById('resultado')
+    campo.innerHTML = `${sorteado}`
+    
+    revelarDiv.classList.add('aposSortear');
+}
+
+
+let revelarDiv = document.querySelector('.revelar');
+
+revelarDiv.addEventListener('click', function() {
+    revelarDiv.classList.add('hide');
+});
